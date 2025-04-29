6f4fbdf2cbc2d9043a0f5a28432a09b09725d7bf
plenilla <fedorovkola2017@gmail.com>
2025-04-29 17:18:38 +0300

    add module.exports for display picture

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,19 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   publicRuntimeConfig: {
-    baseURL: process.env.NEXT_APP_DB_URL
-  }
-
+    baseURL: process.env.NEXT_APP_DB_URL,
+  },
 };
-
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "82.202.137.19",
+        port: "",
+        pathname: "/static/**",
+      },
+    ],
+  },
+};
 export default nextConfig;
