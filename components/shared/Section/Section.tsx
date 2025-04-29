1d540a033b65502e820871c38874256373b7ea5c
plenilla <fedorovkola2017@gmail.com>
2025-04-29 17:19:53 +0300

    delete to exhibition

--- components/shared/Section/Section.tsx
+++ components/shared/Section/Section.tsx
@@ -1,11 +1,9 @@
-import {Exhibitions} from '@/components/shared/Exhibition/Exhibition'
+import {Exhibitions} from '@/components/shared/Exhibition/LinkToExhibitions'
 
 
 export const Section = () => {
     return (
         <div className="flex flex-col items-center gap-5">
-            <Exhibitions/>
-            <Exhibitions/>
             <Exhibitions/>
         </div>
     )
