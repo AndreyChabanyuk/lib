b4fcef7cc80f1f36a48ffee15166a5bf1db6573a
plenilla <fedorovkola2017@gmail.com>
2025-05-01 16:24:14 +0300

    add Backbutton like components

---
+++ components/Mybuttons/BackButton.tsx
@@ -0,0 +1,43 @@
+'use client'; 
+
+import { useRouter } from 'next/navigation';
+import React from 'react';
+import { MdArrowBack } from 'react-icons/md';
+
+interface BackButtonProps {
+  onClick?: () => void;
+  className?: string;
+  customAction?: () => void;
+}
+
+const BackButton = ({ className = '', customAction }: BackButtonProps) => {
+  const router = useRouter();
+  const handleClick = () => {
+    if (customAction){
+        customAction();
+    }else{
+        router.push("/");
+    }
+  }
+    return (
+    <div className={`absolute top-0 text-3xl ${className}`}>
+      <button
+        onClick={handleClick}
+        className="
+          flex flex-row items-center 
+          p-2 rounded-2xl pr-8
+          hover:text-gray-500
+          max-md:hover:text-inherit
+          transition-colors duration-200
+          select-none
+        "
+        aria-label="Назад"
+      >
+        <MdArrowBack className="mx-4 my-2" />
+        Назад
+      </button>
+    </div>
+  );
+};
+
+export default BackButton;
